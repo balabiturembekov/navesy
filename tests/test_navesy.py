2d036bbec15d61f6680ac9bd65f024f3d3f71f8a
Балаби <orfejorfeev@gmail.com>
2021-12-06 20:20:47 +0000

    Добавлен базовый шаблон а также статические файлы, тесты проходят

--- tests/test_navesy.py
+++ tests/test_navesy.py
@@ -1,5 +1,8 @@
-from navesy import __version__
+from navesy.app import app
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_root_url():
+    """ Test: Корневой url обрабатывается правильно """
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Flask App Run'
