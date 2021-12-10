4d730d8bde6eee8169a27b8fa37c4dfbbc1096a8
Balabi94 <orfejorfeev@gmail.com>
2021-12-11 01:29:55 +0600

    Написаны css медиа запросы

--- tests/test_navesy.py
+++ tests/test_navesy.py
@@ -5,4 +5,3 @@ def test_root_url():
     """ Test: Корневой url обрабатывается правильно """
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b'Flask App Run'
