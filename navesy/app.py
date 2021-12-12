1ea19aa96684270de5437ed66ced462be2cbbcb9
Балаби <orfejorfeev@gmail.com>
2021-12-12 21:06:10 +0000

    Модуль wsgi для gunicorn

--- navesy/app.py
+++ navesy/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flask_admin import Admin
 
 from config import Configuration
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
-admin = Admin(app)
+#admin = Admin(app)
 
 
